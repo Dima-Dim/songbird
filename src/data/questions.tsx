c5a56b3a5f97ffed78ff3d87a9fe51dd369cd9a4
Dima <dima@onmaster.ru>
2020-08-22 09:37:48 +0300

    feat: add questions data

---
+++ src/data/questions.tsx
@@ -0,0 +1,484 @@
+import {QuestionsData} from "../types/questions-types";
+
+export const questions:QuestionsData = {
+  FANTASY: {
+    title: {
+      ru: "фэнтези",
+      en: "fantasy",
+    },
+    options: [
+      {
+        audioFile: "f-1.mp3",
+        imageFile: "f-1.jpg",
+        author: {
+          en: "Michael Moorcock",
+          ru: "Майкл Муркок",
+        },
+        name: {
+          en: "Amulet of the Mad God",
+          ru: "Амулет безумного бога",
+        },
+      },
+      {
+        audioFile: "f-2.mp3",
+        imageFile: "f-2.jpg",
+        author: {
+          en: "Clive Staples Lewis",
+          ru: "Клайв Степлз Льюис",
+        },
+        name: {
+          en: "The horse and his boy",
+          ru: "Конь и его мальчик",
+        },
+      },
+      {
+        audioFile: "f-3.mp3",
+        imageFile: "f-3.jpg",
+        author: {
+          en: "Terry Pratchett",
+          ru: "Терри Пратчетт",
+        },
+        name: {
+          en: "Last Hero",
+          ru: "Последний герой",
+        },
+      },
+      {
+        audioFile: "f-4.mp3",
+        imageFile: "f-4.jpg",
+        author: {
+          en: "Clive Staples Lewis",
+          ru: "Клайв Степлз Льюис",
+        },
+        name: {
+          en: "The Chronicles of Narnia: Prince Caspian",
+          ru: "Хроники Нарнии: Принц Каспиан",
+        },
+      },
+      {
+        audioFile: "f-5.mp3",
+        imageFile: "f-5.jpg",
+        author: {
+          en: "J.K. Rowling",
+          ru: "Дж. К. Роулинг",
+        },
+        name: {
+          en: "Harry Potter and the sorcerer's stone",
+          ru: "Гарри Поттер и Философский камень",
+        },
+      },
+      {
+        audioFile: "f-6.mp3",
+        imageFile: "f-6.jpg",
+        author: {
+          en: "Clive Staples Lewis",
+          ru: "Клайв Степлз Льюис",
+        },
+        name: {
+          en: "The sorcerer's nephew",
+          ru: "Племянник чародея",
+        },
+      },
+    ]
+  },
+  CHILDREN: {
+    title: {
+      ru: "детские",
+      en: "children",
+    },
+    options: [
+      {
+        audioFile: "ch-1.mp3",
+        imageFile: "ch-1.jpg",
+        author: {
+          en: "Victor Dragunsky",
+          ru: "Виктор Драгунский",
+        },
+        name: {
+          en: "Deniskin stories",
+          ru: "Денискины рассказы",
+        },
+      },
+      {
+        audioFile: "ch-2.mp3",
+        imageFile: "ch-2.jpg",
+        author: {
+          en: "Sofia Prokofieva",
+          ru: "Софья Прокофьева",
+        },
+        name: {
+          en: "The Mystery of the Crystal Castle",
+          ru: "Тайна Хрустального замка",
+        },
+      },
+      {
+        audioFile: "ch-3.mp3",
+        imageFile: "ch-3.jpg",
+        author: {
+          en: "Lev Kassil",
+          ru: "Лев Кассиль",
+        },
+        name: {
+          en: "Conduit and Schwambrania",
+          ru: "Кондуит и Швамбрания",
+        },
+      },
+      {
+        audioFile: "ch-4.mp3",
+        imageFile: "ch-4.jpg",
+        author: {
+          en: "Linevsky Alexander",
+          ru: "Линевский Александр",
+        },
+        name: {
+          en: "Stone Book Sheets",
+          ru: "Листы каменной книги",
+        },
+      },
+      {
+        audioFile: "ch-5.mp3",
+        imageFile: "ch-5.jpg",
+        author: {
+          en: "Levshin Vladimir Arturovich",
+          ru: "Левшин Владимир Артурович",
+        },
+        name: {
+          en: "Seekers of extraordinary autographs",
+          ru: "Искатели необычайных автографов",
+        },
+      },
+      {
+        audioFile: "ch-6.mp3",
+        imageFile: "ch-6.jpg",
+        author: {
+          en: "Bulychev Kir",
+          ru: "Булычев Кир",
+        },
+        name: {
+          en: "Alice and the Crusaders",
+          ru: "Алиса и крестоносцы",
+        },
+      },
+    ]
+  },
+  DETECTIVES: {
+    title: {
+      ru: "детективы",
+      en: "detectives",
+    },
+    options: [
+      {
+        audioFile: "d-1.mp3",
+        imageFile: "d-1.jpg",
+        author: {
+          en: "Chingiz Abdullaev",
+          ru: "Чингиз Абдуллаев",
+        },
+        name: {
+          en: "Berlin transit",
+          ru: "Берлинский транзит",
+        },
+      },
+      {
+        audioFile: "d-2.mp3",
+        imageFile: "d-2.jpg",
+        author: {
+          en: "Chingiz Abdullaev",
+          ru: "Чингиз Абдуллаев",
+        },
+        name: {
+          en: "Assassin Grand Prix",
+          ru: "Гран-При для убийцы",
+        },
+      },
+      {
+        audioFile: "d-3.mp3",
+        imageFile: "d-3.jpg",
+        author: {
+          en: "Gelbraith Robert",
+          ru: "Гелбрейт Роберт",
+        },
+        name: {
+          en: "Silkworm",
+          ru: "Шелкопряд",
+        },
+      },
+      {
+        audioFile: "d-4.mp3",
+        imageFile: "d-4.jpg",
+        author: {
+          en: "Darya Dontsova",
+          ru: "Дарья Донцова",
+        },
+        name: {
+          en: "Damn out of the snuff box",
+          ru: "Чёрт из табакерки",
+        },
+      },
+      {
+        audioFile: "d-5.mp3",
+        imageFile: "d-5.jpg",
+        author: {
+          en: "Agatha Christie",
+          ru: "Агата Кристи",
+        },
+        name: {
+          en: "Ten Little Niggers",
+          ru: "Десять негритят",
+        },
+      },
+      {
+        audioFile: "d-6.mp3",
+        imageFile: "d-6.jpg",
+        author: {
+          en: "Akunin Boris",
+          ru: "Акунин Борис",
+        },
+        name: {
+          en: "Azazel",
+          ru: "Азазель",
+        },
+      },
+    ]
+  },
+  FANTASTIC: {
+    title: {
+      ru: "фантастика",
+      en: "fantastic",
+    },
+    options: [
+      {
+        audioFile: "fc-1.mp3",
+        imageFile: "fc-1.jpg",
+        author: {
+          en: "King Stephen",
+          ru: "Кинг Стивен",
+        },
+        name: {
+          en: "1408",
+          ru: "1408",
+        },
+      },
+      {
+        audioFile: "fc-2.mp3",
+        imageFile: "fc-2.jpg",
+        author: {
+          en: "King Stephen",
+          ru: "Кинг Стивен",
+        },
+        name: {
+          en: "JoyLand",
+          ru: "Страна Радости",
+        },
+      },
+      {
+        audioFile: "fc-3.mp3",
+        imageFile: "fc-3.jpg",
+        author: {
+          en: "Rothfuss Patrick",
+          ru: "Ротфусс Патрик",
+        },
+        name: {
+          en: "Wind name",
+          ru: "Имя ветра",
+        },
+      },
+      {
+        audioFile: "fc-4.mp3",
+        imageFile: "fc-4.jpg",
+        author: {
+          en: "Jules Verne",
+          ru: "Жюль Верн",
+        },
+        name: {
+          en: "Lord of the world",
+          ru: "Властелин мира",
+        },
+      },
+      {
+        audioFile: "fc-5.mp3",
+        imageFile: "fc-5.jpg",
+        author: {
+          en: "John Wyndham",
+          ru: "Джон Уиндэм",
+        },
+        name: {
+          en: "Day of the Triffids",
+          ru: "День триффидов",
+        },
+      },
+      {
+        audioFile: "fc-6.mp3",
+        imageFile: "fc-6.jpg",
+        author: {
+          en: "Frank Herbert",
+          ru: "Фрэнк Герберт",
+        },
+        name: {
+          en: "Dune",
+          ru: "Дюна",
+        },
+      },
+    ]
+  },
+  NOVELS: {
+    title: {
+      ru: "романы",
+      en: "novels",
+    },
+    options: [
+      {
+        audioFile: "n-1.mp3",
+        imageFile: "n-1.jpg",
+        author: {
+          en: "Elizabeth Gilbert",
+          ru: "Элизабет Гилберт",
+        },
+        name: {
+          en: "Eat, pray, love",
+          ru: "Есть, молиться, любить",
+        },
+      },
+      {
+        audioFile: "n-2.mp3",
+        imageFile: "n-2.jpg",
+        author: {
+          en: "Gregory Philip",
+          ru: "Грегори Филиппа",
+        },
+        name: {
+          en: "Another Boleyn Girl",
+          ru: "Еще одна из рода Болейн",
+        },
+      },
+      {
+        audioFile: "n-3.mp3",
+        imageFile: "n-3.jpg",
+        author: {
+          en: "Arthur Golden",
+          ru: "Артур Голден",
+        },
+        name: {
+          en: "Memoirs of a Geisha",
+          ru: "Мемуары гейши",
+        },
+      },
+      {
+        audioFile: "n-4.mp3",
+        imageFile: "n-4.jpg",
+        author: {
+          en: "Philip Gregory",
+          ru: "Филиппа Грегори",
+        },
+        name: {
+          en: "Weidekr, or Dark Passion",
+          ru: "Вайдекр, или Темная страсть",
+        },
+      },
+      {
+        audioFile: "n-5.mp3",
+        imageFile: "n-5.jpg",
+        author: {
+          en: "Mitchell Margaret",
+          ru: "Митчелл Маргарет",
+        },
+        name: {
+          en: "Gone With the Wind",
+          ru: "Унесенные ветром",
+        },
+      },
+      {
+        audioFile: "n-6.mp3",
+        imageFile: "n-6.jpg",
+        author: {
+          en: "Archibald Cronin",
+          ru: "Арчибальд Кронин",
+        },
+        name: {
+          en: "Brody Castle",
+          ru: "Замок Броуди",
+        },
+      },
+    ]
+  },
+  CLASSIC: {
+    title: {
+      ru: "классика",
+      en: "classic",
+    },
+    options: [
+      {
+        audioFile: "cl-1.mp3",
+        imageFile: "cl-1.jpg",
+        author: {
+          en: "Fedor Dostoevsky",
+          ru: "Фёдор Достоевский",
+        },
+        name: {
+          en: "Someone else's wife and husband under the bed",
+          ru: "Чужая жена и муж под кроватью",
+        },
+      },
+      {
+        audioFile: "cl-2.mp3",
+        imageFile: "cl-2.jpg",
+        author: {
+          en: "Ivan Goncharov",
+          ru: "Иван Гончаров",
+        },
+        name: {
+          en: "An ordinary story",
+          ru: "Обыкновенная история",
+        },
+      },
+      {
+        audioFile: "cl-3.mp3",
+        imageFile: "cl-3.jpg",
+        author: {
+          en: "Nikolay Gogol",
+          ru: "Николай Гоголь",
+        },
+        name: {
+          en: "Viy",
+          ru: "Вий",
+        },
+      },
+      {
+        audioFile: "cl-4.mp3",
+        imageFile: "cl-4.jpg",
+        author: {
+          en: "Alexey Tolstoy",
+          ru: "Алексей Толстой",
+        },
+        name: {
+          en: "Prince Silver",
+          ru: "Князь Серебряный",
+        },
+      },
+      {
+        audioFile: "cl-5.mp3",
+        imageFile: "cl-5.jpg",
+        author: {
+          en: "Nikolay Gogol",
+          ru: "Николай Гоголь",
+        },
+        name: {
+          en: "Portrait",
+          ru: "Портрет",
+        },
+      },
+      {
+        audioFile: "cl-6.mp3",
+        imageFile: "cl-6.jpg",
+        author: {
+          en: "Nikolay Leskov",
+          ru: "Николай Лесков",
+        },
+        name: {
+          en: "Soborians",
+          ru: "Соборяне",
+        },
+      },
+    ]
+  },
+};
