90f9c2d2577e65f98a501f964ee28e598d9bc2fa
Dima <dima@onmaster.ru>
2020-08-28 17:31:01 +0300

    feat: add Modal

---
+++ src/components/modal/sc.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const modalSC = {
+  OVERLAY: styled.section`
+    box-sizing: border-box;
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+  `,
+};
