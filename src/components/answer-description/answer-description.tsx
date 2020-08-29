4db043dc5f98b3d1b8b256b1ce9b368eec918008
Dima <dima@onmaster.ru>
2020-08-29 20:46:55 +0300

    fix: memory leak in answer-description.tsx

--- src/components/answer-description/answer-description.tsx
+++ src/components/answer-description/answer-description.tsx
@@ -5,7 +5,7 @@ import UseGettingTextFromUrl from "../../hooks/use-getting-text-from-url/use-get
 import {answerDescriptionPropsSC as SC} from "./sc";
 
 interface AnswerDescriptionProps {
-  option: QuestionOption;
+  option?: QuestionOption;
 }
 
 const AnswerDescription: React.FC<AnswerDescriptionProps> = (props) => {
@@ -13,30 +13,42 @@ const AnswerDescription: React.FC<AnswerDescriptionProps> = (props) => {
     option,
   } = props;
 
-  const audioSrc = `${assetsUrl.MAIN}/${assetsUrl.AUDIO_FOLDER}/${option.audioFileUrl}`;
-  const imageSrc = `${assetsUrl.MAIN}/${assetsUrl.IMAGE_FOLDER}/${option.imageFileUrl}`;
-  const textSrc = `${assetsUrl.MAIN}/${assetsUrl.TEXT_FOLDER}/${option.textFileUrl.ru}`;
-  const titleRu = option.name.ru;
-  const titleEn = option.name.en;
+  const audioSrc = `${assetsUrl.MAIN}/${assetsUrl.AUDIO_FOLDER}/${option?.audioFileUrl}`;
+  const imageSrc = `${assetsUrl.MAIN}/${assetsUrl.IMAGE_FOLDER}/${option?.imageFileUrl}`;
+  const textSrc = `${assetsUrl.MAIN}/${assetsUrl.TEXT_FOLDER}/${option?.textFileUrl.ru}`;
+  const titleRu = option?.name.ru;
+  const titleEn = option?.name.en;
 
-  const description = UseGettingTextFromUrl({url: textSrc});
+  const description = option && UseGettingTextFromUrl({url: textSrc});
+
+  const content = option
+    ? (
+      <>
+        <img
+          src={imageSrc}
+          alt={titleRu}
+        />
+        <h3>{titleRu} <span lang="en">({titleEn})</span></h3>
+        <SC.AUDIO_PLAYER
+          src={audioSrc}
+          canvasWidth={100}
+          key={audioSrc}
+        />
+        <p>
+          {description}
+        </p>
+      </>
+    )
+    : (
+      <>
+        <p>Выберите верный вариант из списка ответов.</p>
+      </>
+    );
 
   return (
     <SC.CONTAINER>
       <h2>Ваш ответ</h2>
-      <img
-        src={imageSrc}
-        alt={titleRu}
-      />
-      <h3>{titleRu} <span lang="en">({titleEn})</span></h3>
-      <SC.AUDIO_PLAYER
-        src={audioSrc}
-        canvasWidth={100}
-        key={audioSrc}
-      />
-      <p>
-        {description}
-      </p>
+      {content}
     </SC.CONTAINER>
   );
 };
